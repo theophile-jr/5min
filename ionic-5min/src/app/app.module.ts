a463907b49da32232b7fac3af7c9866e76ba49c2
Théophile JR <theophilejeromerocher44@gmail.com>
2023-02-10 17:47:06 +0100

    fix pwa : back appmodule service worker & edit webmanifest

--- ionic-5min/src/app/app.module.ts
+++ ionic-5min/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
   enabled: !isDevMode(),
-  registrationStrategy: 'registerImmediately',
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
 })],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+  { provide: Window, useValue: window }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
